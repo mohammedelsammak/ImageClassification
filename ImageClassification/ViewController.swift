7c5561e5a85f3aaa6ee16e9c46087783e1b06882
Mohammed Elsammak <mohammed.elsammak@alpha-connection.net>
2018-03-02 17:50:17 +0900

    Add video locally

--- ImageClassification/ViewController.swift
+++ ImageClassification/ViewController.swift
@@ -3,14 +3,18 @@ import CoreML
 import Vision
 import ImageIO
 import ARKit
+import AVKit
+import AVFoundation
+
 
 class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
     // MARK: - IBOutlets        
-    @IBOutlet weak var classificationLabel: UILabel!
-    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var classificationLabel: UILabel!    
+    @IBOutlet weak var videoView: UIView!
+    
     
     // MARK: - Image Classification
-    
+    @objc var player: AVPlayer!
     private var scanTimer: Timer?
     private var scannedFaceViews = [UIView]()
     
@@ -37,25 +41,32 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
     //MARK:- Init views
     override func viewDidLoad() {
         super.viewDidLoad()
+
+//        let videoURL = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+        let videoURL = URL(fileURLWithPath: Bundle.main.path(forResource: "Andreas", ofType: "MOV")!)
+        player = AVPlayer(url: videoURL)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        self.videoView.layer.addSublayer(playerLayer)
         
-        sceneView.delegate = self
+        player.play()
+        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: self.player.currentItem)
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let configuration = ARWorldTrackingConfiguration()
-        sceneView.session.run(configuration)
-        
         //scan for faces in regular intervals
         scanTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(scanForFaces), userInfo: nil, repeats: true)
+        
+        self.view.bringSubview(toFront: classificationLabel)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         scanTimer?.invalidate()
-        sceneView.session.pause()
     }
     
     @objc
@@ -64,47 +75,71 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
         //remove the test views and empty the array that was keeping a reference to them
         _ = scannedFaceViews.map { $0.removeFromSuperview() }
         scannedFaceViews.removeAll()
-        
+
         //get the captured image of the ARSession's current frame
-        guard let capturedImage = sceneView.session.currentFrame?.capturedImage else { return }
-        
-        
-        let image = CIImage.init(cvPixelBuffer: capturedImage)
+        guard let capturedImage = screenshotCMTime(cmTime: player.currentTime()) else { return }
+//            guard let capturedImage = sceneView.session.currentFrame?.capturedImage else { return }
+//        imageView.image = capturedImage
+        let image = CIImage(image: capturedImage)!
+
+//        let image = CIImage.init(cvPixelBuffer: capturedImage)
         
         let detectFaceRequest = VNDetectFaceRectanglesRequest { (request, error) in
-            
+
             DispatchQueue.main.async {
                 //Loop through the resulting faces and add a red UIView on top of them.
                 if let faces = request.results as? [VNFaceObservation] {
                     if faces.count == 0 {
-                        self.classificationLabel.text = "Searching for faces..."                        
+                        self.classificationLabel.text = "Searching for faces..."
                     }
                     for face in faces {
                         let faceFrame = self.faceFrame(from: face.boundingBox)
                         let faceView = UIView(frame: faceFrame)
-                        
+
                         let croppedCIimage = image.cropImage(toFace: face).rotate
-                        
-                        
+
+
                         self.updateClassifications(for: croppedCIimage)
-                        
+
                         faceView.backgroundColor = .clear
                         faceView.layer.borderColor = UIColor.yellow.cgColor;
                         faceView.layer.borderWidth = 2;
-                        
-                        
-                        self.sceneView.addSubview(faceView)
-                        
+
+
+                        self.view.addSubview(faceView)
+
                         self.scannedFaceViews.append(faceView)
                     }
                 }
             }
-            
+
         }
         DispatchQueue.global().async {
             try? VNImageRequestHandler(ciImage: image, orientation: self.imageOrientation).perform([detectFaceRequest])
         }
     }
+    
+    func screenshotCMTime(cmTime: CMTime)  -> (UIImage)?
+    {
+        guard let player = player,let asset = player.currentItem?.asset else
+        {
+            return nil
+        }
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        var image: UIImage?
+        var timePicture = kCMTimeZero
+        imageGenerator.appliesPreferredTrackTransform = true
+        imageGenerator.requestedTimeToleranceAfter = kCMTimeZero
+        imageGenerator.requestedTimeToleranceBefore = kCMTimeZero
+        do {
+            let ref = try imageGenerator.copyCGImage(at: cmTime, actualTime: &timePicture)
+            
+            image = UIImage(cgImage: ref)
+        }catch {
+        }
+        return image
+    }
+
     /// - Tag: PerformRequests
     func updateClassifications(for image: CIImage) {
         
@@ -133,7 +168,7 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
             }
             // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
             let classifications = results as! [VNClassificationObservation]
-            print(request.observationInfo)
+            
             if classifications.isEmpty {
                 self.classificationLabel.text = "Nothing recognized."
             } else {
@@ -147,8 +182,8 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
     private func faceFrame(from boundingBox: CGRect) -> CGRect {
         
         //translate camera frame to frame inside the ARSKView
-        let origin = CGPoint(x: boundingBox.minX * sceneView.bounds.width, y: (1 - boundingBox.maxY) * sceneView.bounds.height)
-        let size = CGSize(width: boundingBox.width * sceneView.bounds.width, height: boundingBox.height * sceneView.bounds.height)
+        let origin = CGPoint(x: boundingBox.minX * videoView.bounds.width, y: (1 - boundingBox.maxY) * videoView.bounds.height)
+        let size = CGSize(width: boundingBox.width * videoView.bounds.width, height: boundingBox.height * videoView.bounds.height)
         
         return CGRect(origin: origin, size: size)
     }
@@ -164,6 +199,14 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
         case .faceDown: fallthrough
         case .landscapeLeft: return .up
         }
+    }
+    
+    
+    @objc func playerDidFinishPlaying() {
+        
+        player.seek(to: kCMTimeZero)
+        player.play()
+        
     }
 }
 
