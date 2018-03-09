970c16fa99becebc9ce9b4e30aaf217a3cf55bf2
Mohammed Elsammak <mohammed.elsammak@alpha-connection.net>
2018-03-09 19:31:56 +0900

    Removing unnecessary code

--- ImageClassification/ViewController.swift
+++ ImageClassification/ViewController.swift
@@ -15,14 +15,16 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
     @objc var player: AVPlayer!
     var videoOutput: AVPlayerItemVideoOutput?
     private var scanTimer: Timer?
+    private var startTime = NSDate.timeIntervalSinceReferenceDate
     lazy var mlModel = AlphanoteNew()
     let PREPAREDATA = false
-    
+    let PERCENTAGE_OF_DETECTING = 70   // detecting faces inside 30% of the frames
+    let FPS = 10
     func setUpOutput() {
         let videoItem = player.currentItem!
         if videoItem.status != AVPlayerItemStatus.readyToPlay {
             // see https://forums.developer.apple.com/thread/27589#128476
-            return
+//            return
         }
         
         let pixelBuffAttributes = [
@@ -38,7 +40,7 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
         guard let videoOutput = videoOutput, let currentItem = player.currentItem else { return nil }
         
         let time = currentItem.currentTime()
-        if !videoOutput.hasNewPixelBuffer(forItemTime: time) { return nil }
+//        if !videoOutput.hasNewPixelBuffer(forItemTime: time) { return nil }
         guard let buffer = videoOutput.copyPixelBuffer(forItemTime: time, itemTimeForDisplay: nil)
             else { return nil }
         return buffer
@@ -61,66 +63,58 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
     }
 
     @objc func shotVideo() {
-        if let image = screenshotCMTime(cmTime: player.currentTime()) {
-//            self.imageView.image = image
-            detectFaces(forImage: image)
-        }
+        
+        guard let buffer = getNewFrame() else { return }
+        
+        let ciImage = CIImage(cvPixelBuffer: buffer)
+        let image = UIImage(ciImage: ciImage)
+        
+        
+        changeImage(image: image)
+
     }
     
+    func changeImage(image: UIImage) {
+        
+        let number = arc4random_uniform(100)
+        
+        if number <= PERCENTAGE_OF_DETECTING {
+             detectFaces(forImage: image)
+        } else {
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
+        
+    }
     //MARK:- Init views
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        let videoURL = URL(string: "https://hls.ssh101.com/live/Tokyo/index.m3u8")!;
+        //http://rtmp.streamaxia.com:1935/streamaxia/Senko/playlist.m3u8
+        //http://ec2-13-115-35-165.ap-northeast-1.compute.amazonaws.com/drone.m3u8
+        let videoURL = URL(string: "http://rtmp.streamaxia.com:1935/streamaxia/Senko/playlist.m3u8")!;
         
 //        let videoURL = URL(string: "http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8")!
 //        let videoURL = URL(string: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8")!
 //        let videoURL = URL(string: "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8")!
-        let videoURL = URL(string: "http://ec2-13-115-35-165.ap-northeast-1.compute.amazonaws.com/vod/3.mp4")!
+//        let videoURL = URL(string: "http://ec2-13-115-35-165.ap-northeast-1.compute.amazonaws.com/vod/3.mp4")!
 //        let videoURL = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!
 //        let videoURL = URL(fileURLWithPath: Bundle.main.path(forResource: "Kengo", ofType: "MOV")!)
         player = AVPlayer(url: videoURL)
-        let playerLayer = AVPlayerLayer(player: player)
-        playerLayer.frame = self.view.bounds
-        self.view.layer.addSublayer(playerLayer)
+//        let playerLayer = AVPlayerLayer(player: player)
+//        playerLayer.frame = self.view.bounds
+//        self.view.layer.addSublayer(playerLayer)
         self.view.bringSubview(toFront: imageView)
         player.play()
         
-        scanTimer = Timer.scheduledTimer(timeInterval: (1/240), target: self, selector: #selector(shotVideo), userInfo: nil, repeats: true)
-        
-//        player.addPeriodicTimeObserver(
-//            forInterval: CMTime(value: 1, timescale: 120),
-//            queue: DispatchQueue(label: "videoProcessing", qos: .background),
-//            using: { time in
-//                self.doThingsWithFaces()
-//
-//        })
-////
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-//            self.setUpOutput()
-//        }
-    }
-    
-    func screenshotCMTime(cmTime: CMTime)  -> (UIImage)?
-    {
-        guard let player = player,let asset = player.currentItem?.asset else
-        {
-            return nil
+        scanTimer = Timer.scheduledTimer(timeInterval: (1/10), target: self, selector: #selector(shotVideo), userInfo: nil, repeats: true)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.setUpOutput()
         }
-        let imageGenerator = AVAssetImageGenerator(asset: asset)
-        var image: UIImage?
-        var timePicture = kCMTimeZero
-        imageGenerator.appliesPreferredTrackTransform = true
-        imageGenerator.requestedTimeToleranceAfter = kCMTimeZero
-        imageGenerator.requestedTimeToleranceBefore = kCMTimeZero
-        do {
-            let ref = try imageGenerator.copyCGImage(at: cmTime, actualTime: &timePicture)
-            
-            image = UIImage(cgImage: ref)
-        }catch {
-        }
-        return image
     }
+
     
     func crop(image: UIImage, rect: CGRect)-> UIImage? {
         UIGraphicsBeginImageContextWithOptions(rect.size, false, image.scale)
@@ -189,12 +183,9 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
                 self.imageView.image = final_image
             }
         }
-        
-        
-        let ciimage = CIImage(cgImage: image.cgImage!)
-//        guard let ciimage = CIImage(cgImage: image.cgImage!) else{
-//            fatalError("couldn't convert uiimage to ciimage")
-//        }
+        guard let ciimage = image.ciImage else{
+            fatalError("couldn't convert uiimage to ciimage")
+        }
         
         let handler=VNImageRequestHandler(ciImage: ciimage)
         DispatchQueue.global(qos: .userInteractive).async{
@@ -226,41 +217,6 @@ class ImageClassificationViewController: UIViewController, ARSCNViewDelegate {
         return predictionValue
     }
     
-    func imageWithPixelSize(size: CGSize, filledWithColor color: UIColor = UIColor.clear, opaque: Bool = false) -> UIImage {
-        return imageWithSize(size: size, filledWithColor: color, scale: 1.0, opaque: opaque)
-    }
-    
-    func imageWithSize(size: CGSize, filledWithColor color: UIColor = UIColor.clear, scale: CGFloat = 0.0, opaque: Bool = false) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        
-        UIGraphicsBeginImageContextWithOptions(size, opaque, scale)
-        color.set()
-        UIRectFill(rect)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return image!
-    }
-   
-    
-    func image(fromVideo videoURL: URL?, atTime time: TimeInterval) -> UIImage? {
-        // courtesy of memmons
-        // http://stackoverflow.com/questions/1518668/grabbing-the-first-frame-of-a-video-from-uiimagepickercontroller
-        let asset = AVURLAsset(url: videoURL!, options: nil)
-        
-        let assetIG = AVAssetImageGenerator(asset: asset)
-        assetIG.appliesPreferredTrackTransform = true
-        assetIG.apertureMode = .encodedPixels
-        var thumbnailImageRef: CGImage? = nil
-        let thumbnailImageTime = CFTimeInterval(time)
-        let igError: Error? = nil
-        
-        thumbnailImageRef = try! assetIG.copyCGImage(at: CMTimeMake(Int64(thumbnailImageTime), 60), actualTime: nil)
-        
-        var image = UIImage(cgImage: thumbnailImageRef!)
-        
-        return image
-    }
 }
 
 
